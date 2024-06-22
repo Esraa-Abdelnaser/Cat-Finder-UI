be45ff91726981edb062e3107149326440b89cde
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2024-06-22 04:33:38 +0300

    add signup and contact pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./components/footer/footer.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {SignupComponent} from "./components/signup/signup.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SignupComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
